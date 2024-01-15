81f91249528814716ef0b24d7bc0ad82e53c75ca
ManonC12 <manon-c.coutier@wanadoo.fr>
2024-01-15 14:23:10 +0100

    download fct upgraded

--- midi_transformer/dataset.py
+++ midi_transformer/dataset.py
@@ -5,17 +5,25 @@ import pandas as pd
 import os
 
 
-"""def download_dataset():
+def download_dataset():
     examples, metadata = tfds.load(
         "ted_hrlr_translate/pt_to_en", with_info=True, as_supervised=True
     )
 
-    return examples["train"], examples["validation"]"""
+    return examples["train"], examples["validation"]
 
 
 def download_maestro_dataset():
     """
-    Downloads MAESTRO MIDI dataset in a data/maestro-v.3.0.0-midi
+    Downloads MAESTRO MIDI dataset in a data/maestro-v.3.0.0-midi.
+    Returns a train/test/validation split of the MIDIs.
+
+    Returns:
+        tuple[list[pathlib.Path]]: tuple of three lists of paths to the MIDIs.
+                                   First contains the paths of the MIDIs for the train dataset,
+                                   second contains the paths of the MIDIs for the test dataset,
+                                   third contains the paths of the MIDIs for the validation dataset,
+
     """
     data_dir = pathlib.Path("data/maestro-v3.0.0")
     if not data_dir.exists():
@@ -30,8 +38,32 @@ def download_maestro_dataset():
 
     csv_dir = pathlib.Path("data/maestro-v3.0.0/maestro-v3.0.0.csv")
     midi_df = pd.read_csv(csv_dir, sep=",")
-    train_df = midi_df.loc[midi_df["split"] == "train"]
-    test_df = midi_df.loc[midi_df["split"] == "test"]
-    validation_df = midi_df.loc[midi_df["split"] == "validation"]
 
-    # TODO : list de pathlib.path de chaque truc
+    train_df = midi_df["midi_filename"].loc[midi_df["split"] == "train"]
+    test_df = midi_df["midi_filename"].loc[midi_df["split"] == "test"]
+    validation_df = midi_df["midi_filename"].loc[midi_df["split"] == "validation"]
+
+    train_midi = extract_path(train_df, data_dir)
+    test_midi = extract_path(test_df, data_dir)
+    validation_midi = extract_path(validation_df, data_dir)
+
+    return train_midi, test_midi, validation_midi
+
+
+def extract_path(dataset: pd.Series, data_dir: pathlib.Path):
+    """
+    From a dataset of paths, returns a list the absolute paths corresponding.
+
+    Args:
+        dataset (pd.Series): series containing paths to MIDIs
+        data_dir (pathlib.Path): path to the folder where all MIDIs are
+
+    Returns:
+        list[pathlib.Path] : list of all absolute paths to MIDIs
+    """
+    list_path = []
+
+    for path in dataset:
+        list_path.append(pathlib.Path(os.path.abspath(data_dir.joinpath(path))))
+
+    return list_path
