694bcc2f32e7e10618d5fd088e7682aa75e32380
rravelli <remi.ravelli@gmail.com>
2024-01-15 13:22:21 +0100

    feat: make_midi_batches

--- midi_transformer/tokenizer.py
+++ midi_transformer/tokenizer.py
@@ -4,10 +4,14 @@ import pretty_midi
 import collections
 import pandas as pd
 import numpy as np
+from pathlib import Path
 
 MAX_TOKENS = 128
 BUFFER_SIZE = 20000
 BATCH_SIZE = 64
+VOCAB_SIZE = 128
+SEQ_LENGTH = 25
+KEY_ORDER = ["pitch", "step", "duration"]
 
 
 # Download tokenizer model
@@ -92,7 +96,9 @@ def midi_to_notes(midi_file: str) -> pd.DataFrame:
     notes = collections.defaultdict(list)
 
     # Sort the notes by start time
-    sorted_notes = sorted(instrument.notes[:MAX_NOTE], key=lambda note: note.start)
+    sorted_notes = sorted(
+        instrument.notes[:MAX_NOTE], key=lambda note: note.start
+    )
     prev_start = sorted_notes[0].start
 
     for note in sorted_notes:
@@ -105,4 +111,71 @@ def midi_to_notes(midi_file: str) -> pd.DataFrame:
         notes["duration"].append(end - start)
         prev_start = start
 
-    return pd.DataFrame({name: np.array(value) for name, value in notes.items()})
+    return pd.DataFrame(
+        {name: np.array(value) for name, value in notes.items()}
+    )
+
+
+def create_sequences(
+    dataset: tf.data.Dataset,
+    seq_length: int,
+    vocab_size=128,
+) -> tf.data.Dataset:
+    """Returns TF Dataset of sequence and label examples."""
+    seq_length = seq_length + 1
+
+    # Take 1 extra for the labels
+    windows = dataset.window(
+        seq_length, shift=1, stride=1, drop_remainder=True
+    )
+
+    # `flat_map` flattens the" dataset of datasets" into a dataset of tensors
+    def flatten(ds: tf.data.Dataset):
+        return ds.batch(seq_length, drop_remainder=True)
+
+    sequences = windows.flat_map(flatten)
+
+    # Normalize note pitch
+    def scale_pitch(x):
+        x = x / [vocab_size, 1.0, 1.0]
+        return x
+
+    # Split the labels
+    def split_labels(sequences):
+        inputs = sequences[:-1]
+        labels_dense = sequences[-1]
+        labels = {key: labels_dense[i] for i, key in enumerate(KEY_ORDER)}
+
+        return scale_pitch(inputs), labels
+
+    return sequences.map(split_labels, num_parallel_calls=tf.data.AUTOTUNE)
+
+
+def make_midi_batches(
+    file_paths: list[Path],
+    num_files: int = 300,
+):
+    all_notes = []
+    for f in file_paths[:num_files]:
+        notes = midi_to_notes(f)
+        all_notes.append(notes)
+
+    all_notes = pd.concat(all_notes)
+
+    train_notes = np.stack([all_notes[key] for key in KEY_ORDER], axis=1)
+
+    notes_ds = tf.data.Dataset.from_tensor_slices(train_notes)
+    notes_ds.element_spec
+
+    seq_ds = create_sequences(notes_ds, SEQ_LENGTH, VOCAB_SIZE)
+
+    buffer_size = (
+        len(all_notes) - SEQ_LENGTH
+    )  # the number of items in the dataset
+    train_ds = (
+        seq_ds.shuffle(buffer_size)
+        .batch(BATCH_SIZE, drop_remainder=True)
+        .cache()
+        .prefetch(tf.data.experimental.AUTOTUNE)
+    )
+    return train_ds
