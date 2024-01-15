66aeba12561488c70ff3939e70f292a83a868f26
momo838 <morgane0044@sfr.fr>
2024-01-15 15:42:12 +0100

    chg test_utils.py

--- midi_transformer/test/test_utils.py
+++ midi_transformer/test/test_utils.py
@@ -1,12 +1,14 @@
 import unittest
 import os
 from utils import midi_to_wav, print_accuracy_and_loss
+import matplotlib.pyplot as plt
+import matplotlib.testing.decorators as mptd
 
 class TestUtils(unittest.TestCase):
 
     def test_midi_to_wav(self):
         # Test the conversion of a MIDI file to WAV
-        midi_file = "/home/momo/piiatransf/piiia-midi-completion/midi_transformer/test/fixture/MIDI-Unprocessed_Recital9-11_MID--AUDIO_11_R1_2018_wav--1.midi"  # Replace with an actual MIDI file path
+        midi_file = "/home/momo/piiatransf/piiia-midi-completion/midi_transformer/test/fixture/MIDI-Unprocessed_Recital9-11_MID--AUDIO_11_R1_2018_wav--1.midi"
         output_name = "test_output"
         midi_to_wav(midi_file, output_name)
 
@@ -16,6 +18,14 @@ class TestUtils(unittest.TestCase):
 
         # Clean up: remove the test output file
         os.remove(wav_file_path)
+    
+    @mptd.image_comparison(baseline_images=['accuracy_and_loss_plots'], extensions=['png'])
+    def test_print_accuracy_and_loss(self):
+        # Test the printing of accuracy and loss graphs
+        history_path = "/home/momo/piiatransf/piiia-midi-completion/midi_transformer/test/fixture/"
+
+        # Call the function and generate the plots
+        print_accuracy_and_loss(history_path)
 
 if __name__ == '__main__':
     unittest.main()
