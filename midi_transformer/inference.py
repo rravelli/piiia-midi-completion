fba6b6dfd463476f9bec490fad15867646e56b78
ManonC12 <manon-c.coutier@wanadoo.fr>
2024-01-26 16:53:39 +0100

    MIDIGenerator template

--- midi_transformer/inference.py
+++ midi_transformer/inference.py
@@ -5,8 +5,8 @@ import pickle
 from transformers import TFAutoModel
 from tokenizer import midi_to_notes
 from utils import midi_to_wav
-
-SEQ_LENGTH = 25
+from tokenizer import SEQ_LENGTH, create_sequences, KEY_ORDER, VOCAB_SIZE
+import numpy as np
 
 
 def notes_to_midi(
@@ -70,3 +70,58 @@ def lets_try_this_baby():
     midi_to_wav(midi_sample, output)
 
     generate_music(midi_sample, "", "music_generated_test")
+
+
+class MIDIGenerator(tf.Module):
+    def __init__(self, transformer):
+        self.transformer = transformer
+
+    def __call__(self, melody: pd.DataFrame, max_length=SEQ_LENGTH):
+        notes = np.stack([melody[key] for key in KEY_ORDER], axis=1)
+        notes_ds = tf.data.Dataset.from_tensor_slices(notes)
+
+        melody = create_sequences(notes_ds, SEQ_LENGTH, VOCAB_SIZE)
+        # melody = self.tokenizers.pt.tokenize(melody).to_tensor()
+
+        encoder_input = melody
+
+        # As the output language is English, initialize the output with the
+        # English `[START]` token.
+        start_end = self.tokenizers.en.tokenize([""])[0]
+        start = start_end[0][tf.newaxis]
+        end = start_end[1][tf.newaxis]
+
+        # `tf.TensorArray` is required here (instead of a Python list), so that the
+        # dynamic-loop can be traced by `tf.function`.
+        output_array = tf.TensorArray(dtype=tf.int64, size=0, dynamic_size=True)
+        output_array = output_array.write(0, start)
+
+        for i in tf.range(max_length):
+            output = tf.transpose(output_array.stack())
+            predictions = self.transformer([encoder_input, output], training=False)
+
+            # Select the last token from the `seq_len` dimension.
+            predictions = predictions[:, -1:, :]  # Shape `(batch_size, 1, vocab_size)`.
+
+            predicted_id = tf.argmax(predictions, axis=-1)
+
+            # Concatenate the `predicted_id` to the output which is given to the
+            # decoder as its input.
+            output_array = output_array.write(i + 1, predicted_id[0])
+
+            if predicted_id == end:
+                break
+
+        output = tf.transpose(output_array.stack())
+        # The output shape is `(1, tokens)`.
+        text = tokenizers.en.detokenize(output)[0]  # Shape: `()`.
+
+        tokens = tokenizers.en.lookup(output)[0]
+
+        # `tf.function` prevents us from using the attention_weights that were
+        # calculated on the last iteration of the loop.
+        # So, recalculate them outside the loop.
+        self.transformer([encoder_input, output[:, :-1]], training=False)
+        attention_weights = self.transformer.decoder.last_attn_scores
+
+        return text, tokens, attention_weights
