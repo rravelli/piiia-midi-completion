4e6b593d420f2ede88a44f038176f90d24816785
rravelli <remi.ravelli@gmail.com>
2024-02-02 14:06:00 +0100

    feat: new tokenizer

--- midi_transformer/tokenizer.py
+++ midi_transformer/tokenizer.py
@@ -1,20 +1,20 @@
-import tensorflow as tf
-import numpy as np
+import json
 from pathlib import Path
+
+import numpy as np
+import tensorflow as tf
 from miditok import REMI, TokenizerConfig
 from symusic import Score
 from tqdm import tqdm
 
-MAX_TOKENS = 128
 BUFFER_SIZE = 20000
 BATCH_SIZE = 64
 SEQ_LENGTH = 64
-TOKENIZER_CONFIG = TokenizerConfig(
-    num_velocities=16, use_chords=True, use_programs=True
-)
+with open("tokenizer.json") as f:
+    config = json.load(f)
+TOKENIZER_CONFIG = TokenizerConfig(use_programs=True, **config)
 TOKENIZER = REMI(TOKENIZER_CONFIG)
 VOCAB_SIZE = len(TOKENIZER.vocab)
-MAX_NOTE = 100  # max number of notes taken from MIDI file
 
 
 def split_labels(sequences):
@@ -39,13 +39,12 @@ def make_midi_batchesv2(
     all_notes = []
     for file in tqdm(file_paths[:num_files]):
         midi = Score(file)
-        tokens = tokenizer(midi).ids
+        tokens = tokenizer.midi_to_tokens(midi).ids
         for i in range(0, len(tokens) - 2 * SEQ_LENGTH, 2 * SEQ_LENGTH):
             sample = np.array(tokens[i : i + 2 * SEQ_LENGTH])
             all_notes.append(sample)
 
     train_notes = np.array(all_notes)
-
     sequences = tf.data.Dataset.from_tensor_slices(train_notes)
     seq_ds = sequences.map(split_labels, num_parallel_calls=tf.data.AUTOTUNE)
 
@@ -53,7 +52,6 @@ def make_midi_batchesv2(
     train_ds = (
         seq_ds.shuffle(buffer_size)
         .batch(BATCH_SIZE, drop_remainder=True)
-        .cache()
         .prefetch(tf.data.experimental.AUTOTUNE)
     )
     return train_ds
